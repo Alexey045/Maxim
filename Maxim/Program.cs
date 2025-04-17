2a6f2f98c946d074c2d691f25837327a98c0485f
Alexey <krivonosov2004045@gmail.com>
2025-04-17 18:33:54 +0500

    Add project files.

---
+++ Maxim/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Maxim
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = Console.ReadLine();
+
+			Console.WriteLine(input.Length % 2 == 0
+				? string.Concat(Reverse(input[..(input.Length / 2)]), Reverse(input[(input.Length / 2)..]))
+				: string.Concat(Reverse(input), input));
+		}
+
+		static string Reverse(string input)
+		{
+			var sb = new StringBuilder(input.Length);
+
+			for (int i = input.Length - 1; i > -1; i--)
+			{
+				sb.Append(input[i]);
+			}
+
+			return sb.ToString();
+		}
+	}
+}
